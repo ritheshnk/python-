e9a4854300351b967aa405bfae3b5a27439a8d9e
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2023-02-21 23:30:49 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,9 @@
+a='Python for beginners'
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.find('f'))
+print(a.replace('for','4'))
+print(a) #apllying string operatio dosent change the orginal string
+print('Python'in  a) #to check python is present in a will return true if present
+
