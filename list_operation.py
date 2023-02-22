8aef81e9bba78445735f8de0d898b3a756dbaed5
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2023-02-22 23:07:57 +0530

    Add files via upload

---
+++ list_operation.py
@@ -0,0 +1,11 @@
+numbers=[1,2,3,4,5]
+print(numbers)
+numbers.append(6) #append num end of list
+print(numbers)
+
+numbers.insert(6,7) # insert at req position
+print(numbers)
+numbers.insert(0,0)
+print(numbers)
+print(1 in numbers)
+print(len(numbers))
