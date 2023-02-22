4b4470cc17722f40a445010fc29bc11c407e1a4b
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2023-02-22 21:28:28 +0530

    Add files via upload

--- weight_converter.py
+++ weight_converter.py
@@ -2,7 +2,8 @@ weight=int(input("weight: "))
 unit=input(" Did you enter your weight in Kg(K) or Pounds(P): ")
 if unit.lower()=="K":
     result=weight/0.45
-    print(str(result)+" lbs")
+    print(str(result)+" lbs") #need to convert result to str cause python doesnt combine int with str
+
 else:
     result=weight*0.453
-    print(str(result)+" Kg")
+    print(str(result)+" Kg") #need to convert result to str cause python doesnt combine int with str
