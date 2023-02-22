db36898ef79f3525bd44dbf46e3e14dd296d6d28
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2023-02-22 23:18:24 +0530

    Add files via upload

---
+++ for_and_while_loop.py
@@ -0,0 +1,11 @@
+numbers=[1,2,3,4,5]
+print("using for loop")
+for i in numbers:
+    print(i)
+
+# for while loop
+print("using while loop")
+i=0
+while i<len(numbers):
+    print(numbers[i])
+    i+=1
